6a1149821c40d426b079647aa7a25aab2ed1b5dc
Alejandro Paz <alepazte@hotmail.com>
2018-05-29 16:11:44 -0600

    cambio del estado de los en el evento onchange del input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
-class SearchBar extends React.Component {
-    render() {
-        return <input onChange={this.onInputChange} />;
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
     }
 
-    onInputChange(event) {
-        console.log(event.target.value);
+    render() {
+        return (
+            <div>
+                <input onChange={event => this.setState({ term: event.target.value }) } />
+            </div>
+        );
     }
 }
 
