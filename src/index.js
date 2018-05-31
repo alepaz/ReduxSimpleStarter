e5aeb481c5f70d5a20b2906cda84dc7a209155a6
Alejandro Paz <alepazte@hotmail.com>
2018-05-31 14:42:56 -0600

    YTSearch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom' //Si deseas renderizar un elemento en el DOM esta es tu libreria
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyAvTFfHbcB-V0zwZafPP32nGo5hsEvKVZY';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+    console.log(data)
+});
 
 //React is a JS library to produce html code and show in the browser
 //Components are snippets of code who produces html code
